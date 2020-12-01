ea3ce8d41993030a3bdbc3b141ded3867ca86aa0
neobyte13 <neobyte13@gmail.com>
2020-12-01 16:15:01 +0100

    change FB init method

--- lib/main.dart
+++ lib/main.dart
@@ -10,56 +10,33 @@ import 'package:GberaaDelivery/screens/shipment_create.dart';
 import 'package:flutter/material.dart';
 import 'package:GberaaDelivery/screens/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        // Initialize FlutterFire:
-        future: _initialization,
-        builder: (context, snapshot) {
-          // Check for errors
-          if (snapshot.hasError) {
-            return Scaffold(
-              body: Center(
-                child: Text('Error. Turn on internet'),
-              ),
-            );
-          }
-
-          // Once complete, show your application
-          if (snapshot.connectionState == ConnectionState.done) {
-            return MaterialApp(
-                title: 'Gberaa Delivery',
-                debugShowCheckedModeBanner: false,
-                theme: ThemeData(
-                  primarySwatch: Colors.orange,
-                  visualDensity: VisualDensity.adaptivePlatformDensity,
-                ),
-                initialRoute: '/',
-                routes: {
-                  '/': (context) => OnBoardingPage(),
-                  '/login': (context) => LoginScreen(),
-                  '/signup': (context) => SignupScreen(),
-                  '/homescreen': (context) => HomeScreen(),
-                  '/newshimpent': (context) => NewShipments(),
-                  '/viewshipments': (context) => ViewShipments(),
-                  '/shipmentcreate': (context) => ShipmentCreate(),
-                  '/trackshipments': (context) => TrackShipments(),
-                  '/location': (context) => LocationSelect(),
-                });
-          }
-
-          // Otherwise, show something whilst waiting for initialization to complete
-          return Center(
-            child: CircularProgressIndicator(
-              backgroundColor: Colors.orange,
-            ),
-          );
+    return MaterialApp(
+        title: 'Gberaa Delivery',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => OnBoardingPage(),
+          '/login': (context) => LoginScreen(),
+          '/signup': (context) => SignupScreen(),
+          '/homescreen': (context) => HomeScreen(),
+          '/newshimpent': (context) => NewShipments(),
+          '/viewshipments': (context) => ViewShipments(),
+          '/shipmentcreate': (context) => ShipmentCreate(),
+          '/trackshipments': (context) => TrackShipments(),
+          '/location': (context) => LocationSelect(),
         });
   }
 }
