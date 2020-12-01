b01b75576f1c854585a1c4702f0f99dd8eb35fa7
neobyte13 <neobyte13@gmail.com>
2020-12-01 14:14:45 +0100

    fixed home screen appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,96 +20,103 @@ class HomeScreen extends StatelessWidget {
                 right: maxSize.width * 0.04,
                 bottom: maxSize.height * 0.04,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(
-                        Icons.menu,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.my_location,
-                        color: Colors.white,
-                        size: 13.0,
-                      ),
-                      SizedBox(
-                        width: maxSize.width * 0.02,
-                      ),
-                      DropDownData(
-                        category: [
-                          'Lagos',
-                          'Ogun',
-                        ],
-                        elevation: 0,
-                        labelText: 'Location',
-                        iconColor: Colors.white,
-                        labelstyle: kNormalText,
-                        itemStyle: kNormalText.copyWith(
-                          color: Colors.white,
-                          fontSize: 10,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Center(
-                    child: Column(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          'Send Your Package Anywhere!',
-                          style: kMainHeading.copyWith(
-                              color: Colors.white, fontSize: 20.0),
+                        Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                        Icon(
+                          Icons.notifications_on,
+                          color: kMainColor,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.my_location,
+                          color: Colors.white,
+                          size: 13.0,
                         ),
                         SizedBox(
-                          height: maxSize.height * 0.01,
+                          width: maxSize.width * 0.02,
                         ),
-                        /*Text(
-                          'Track your shipment in one click',
-                          style: kLightGreyText.copyWith(fontSize: 15.0),
-                        ),**/
-                      ],
-                    ),
-                  ),
-                  /*Padding(
-                    padding: EdgeInsets.only(top: 18.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          width: maxSize.width * 0.65,
-                          height: maxSize.height * 0.06,
-                          decoration: BoxDecoration(
+                        DropDownData(
+                          category: [
+                            'Lagos',
+                            'Ogun',
+                          ],
+                          elevation: 0,
+                          labelText: 'Location',
+                          iconColor: Colors.white,
+                          labelstyle: kNormalText,
+                          itemStyle: kNormalText.copyWith(
                             color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
-                            child: CustomInputText(),
-                          ),
-                        ),
-                        Container(
-                          width: maxSize.width * 0.15,
-                          height: maxSize.height * 0.06,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Icon(
-                            Icons.arrow_forward_ios,
-                            color: kMainColor,
+                            fontSize: 10,
                           ),
                         ),
                       ],
                     ),
-                  ),**/
-                ],
+                    Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            'Send Your Package Anywhere!',
+                            style: kMainHeading.copyWith(
+                                color: Colors.white, fontSize: 20.0),
+                          ),
+                          SizedBox(
+                            height: maxSize.height * 0.01,
+                          ),
+                          /*Text(
+                            'Track your shipment in one click',
+                            style: kLightGreyText.copyWith(fontSize: 15.0),
+                          ),**/
+                        ],
+                      ),
+                    ),
+                    /*Padding(
+                      padding: EdgeInsets.only(top: 18.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: maxSize.width * 0.65,
+                            height: maxSize.height * 0.06,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 20.0),
+                              child: CustomInputText(),
+                            ),
+                          ),
+                          Container(
+                            width: maxSize.width * 0.15,
+                            height: maxSize.height * 0.06,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              color: kMainColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),**/
+                  ],
+                ),
               ),
             )),
             Expanded(
